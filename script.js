4e5d339d0dbd8e2f4e14717af09561175088100d
Aurelie Raout <aurelieraout@gmail.com>
2026-01-09 16:44:01 +0100

    burger menu animation active

--- script.js
+++ script.js
@@ -1,49 +1,5 @@
-// require('dotenv').config();
-// const fetch = require('node-fetch'); // Version 2 de node-fetch
-
-// const API_NINJAS_KEY = process.env.API_NINJAS_KEY;
-
-const apiUrl = "https://api.api-ninjas.com/v1/caloriesburned";
-
-const options = {
-  method: "GET",
-  headers: {
-    "X-Api-Key": API_NINJAS_KEY
-  }
-};
-
-async function getCalories() {
-  try {
-    const sport = document.getElementById("sport").value;
-    const duration = document.getElementById("duration").value;
-    const weight = document.getElementById("weight").value;
-    const weightInPound = Math.ceil(weight*2.20462); // on convertit les Kgs en Pounds, arrondi
-
-    const url = `${apiUrl}?activity=${sport}&duration=${duration}&weight=${weightInPound}`;
-
-    console.log("Sport :", sport);
-    console.log("Durée :", duration);
-    console.log("Poids :", weight);
-
-    const response = await fetch(url, options);
-    if (!response.ok) {
-      throw new Error(`Erreur : ${response.status}`);
-    }
-    const data = await response.json();
-    const calories = data[0].calories_per_hour;
-    console.log("Réponse complète de l'API :", data);
-    console.log("Calories brûlées :", calories);
-
-    // URL avec les données
-    const resultUrl = `resultats.html?calories=${encodeURIComponent(calories)}&sport=${encodeURIComponent(sport)}&duration=${encodeURIComponent(duration)}`;
-    window.location.href = resultUrl; // Redirige vers la page résultats
-  } catch (error) {
-    console.error("Erreur :", error);
-  }
-}
-
-caloriesForm.addEventListener("submit", function(event) {
-  event.preventDefault();
-  getCalories();
-
+const burgerMenu = document.getElementById('burger-menu');
+const menu = document.getElementById('menu');
+burgerMenu.addEventListener('click', () => {
+  menu.classList.toggle('active');
 });
