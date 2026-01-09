13d6b7f1f13400f9785f4d984af9d7fe3b1ddddd
Aurelie Raout <aurelieraout@gmail.com>
2026-01-09 10:55:14 +0100

    fixed if there's no drink with amount of calories. added comma and space for ingredients

--- resultats.js
+++ resultats.js
@@ -27,29 +27,41 @@ function getSuggestedRecipes(caloriesBurned) {
   let postTrainingDrink = [];
   if (caloriesBurned < 250) {
     preMinCalories = 0; preMaxCalories = 50;
-    postMinCalories = 150; postMaxCalories = 250;
+    postMinCalories = 100; postMaxCalories = 250;
 
   } else if (caloriesBurned <400){
     preMinCalories = 50; preMaxCalories = 100;
-    postMinCalories = 250; postMaxCalories = 350;
+    postMinCalories = 250; postMaxCalories = 400;
 
-  } else if (caloriesBurned >400){
-    preMinCalories = 100; preMaxCalories = 400;
-    postMinCalories = 350; postMaxCalories = 500;
+  } else if (caloriesBurned < 600){
+    preMinCalories = 100; preMaxCalories = 300;
+    postMinCalories = 400; postMaxCalories = 600;
+
+  } else if (caloriesBurned > 600){
+    preMinCalories = 300; preMaxCalories = 400;
+    postMinCalories = 600; postMaxCalories = 1000;
   }
 
   preTrainingDrink = preTrainingDrinkAll.filter((drink) => drink.calories >= preMinCalories && drink.calories <= preMaxCalories);
+  // si n'existe pas return pre avec max calories
+  if (preTrainingDrink.length === 0) {
+    preTrainingDrink = preTrainingDrinkAll.filter((drink) => drink.calories <= preMaxCalories);
+  }
   const randomPre = Math.floor(Math.random() * preTrainingDrink.length);
   const selectedPreDrink = preTrainingDrink[randomPre];
   console.log(selectedPreDrink)
   postTrainingDrink = postTrainingDrinkAll.filter((drink) => drink.calories >= postMinCalories && drink.calories <= postMaxCalories);
+  // si n'existe pas return post avec max calories
+  if (postTrainingDrink.length === 0) {
+    postTrainingDrink = postTrainingDrinkAll.filter((drink) => drink.calories <= postMaxCalories);
+  }
   const randomPost = Math.floor(Math.random() * postTrainingDrink.length);
   console.log(postTrainingDrink[randomPost]);
 
 
   recipeName.innerHTML = `<h4><strong>${selectedPreDrink.name}</h4></p>`;
   recipeCalories.innerHTML = `<p>Calories: ${selectedPreDrink.calories}</p>`;
-  recipeIngredients.innerHTML = `<p>Ingredients: ${selectedPreDrink.ingredients}</p>`;
+  recipeIngredients.innerHTML = `<p>Ingredients: ${selectedPreDrink.ingredients.join(", ")}</p>`;
   recipeImage.setAttribute("src", `${selectedPreDrink.image}`);
   console.log(recipeImage)
 
